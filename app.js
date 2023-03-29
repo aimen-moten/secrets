5588e825a4bd6c855f03b10e3601de58eabc09e7
Aimen Moten <aimen.moten@gmail.com>
2023-03-29 19:38:55 -0400

    level 3 security

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const ejs =  require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+// const md5 = require("md5");  -- used to hash passwords
+// const encrypt = require("mongoose-encryption");  -- used to encrypt passwords
 
 const app = express();
 app.use(express.static("public"));
@@ -19,7 +22,8 @@ const userSchema =  new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); -- encryption of password
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -37,28 +41,39 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-
-    try {
-        newUser.save();
-        res.render("secrets");
-    } catch (err) {
-        console.log(err);
-    }
+    
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+            // password: md5(req.body.password)  //md5 will hash the password and save it to our database
+        });
+    
+        try {
+            newUser.save();
+            res.render("secrets");
+        } catch (err) {
+            console.log(err);
+        }
+    });
     
 });
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    // const password = md5(req.body.password);  //we are hashing the password entered by the user and checking it with the hashed password that is saved in our database
     try {
         const resp = await User.findOne({email: username});
-        if (resp.password === password){
-            res.render("secrets");
-        }
+        bcrypt.compare(password, resp.password, function(err, result) {
+            if (result === true){
+                res.render("secrets");
+            }
+        });
+        
+        // if (resp.password === password){
+        //     res.render("secrets");
+        // }   --- code used when using md5
     } catch (err) {
         console.log(err)
     }
